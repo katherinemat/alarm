1928ef982ba440116702fe537636b87f80bdd798
Kaz Matthews and Sandro Mijatovic <kaz+sandro@>
2017-03-14 15:05:46 -0700

    seperate code better into another prototype called getAlarmTime and comparing that return value against the current time.

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -4,26 +4,11 @@ var Alarm = require('./../js/alarm.js').alarmModule;
 $(document).ready(function() {
   var alarm = new Alarm();
   var alarmTime;
-  var alarmHours;
-  var alarmMinutes;
-  var alarmM;
+
   $('form#alarm').submit(function(event) {
     event.preventDefault();
-    alarmTime = $('#alarm-time').val().toString();
-
-    alarmHours = parseInt(alarmTime.substring(0,2));
-    if (alarmHours > 12) {
-      alarmHours -= 12;
-      alarmM = "PM";
-    }
-    else {
-      alarmM = "AM";
-    }
-    alarmHours.toString();
-
-    alarmMinutes = alarmTime.substring(3);
-
-    alarmTime = alarmHours + ":" + alarmMinutes + ":00 " + alarmM;
+    var input = $('#alarm-time').val().toString();
+    alarmTime = alarm.getAlarmTime(input);
   });
 
   setInterval(function(){
--- js/alarm.js
+++ js/alarm.js
@@ -7,4 +7,25 @@ Alarm.prototype.getTime = function() {
   return moment().format('LTS');
 }
 
+Alarm.prototype.getAlarmTime = function(time) {
+  var alarmTime = time;
+
+  var alarmHours = parseInt(alarmTime.substring(0,2));
+  var alarmM;
+
+  if (alarmHours > 12) {
+    alarmHours -= 12;
+    alarmM = "PM";
+  }
+  else {
+    alarmM = "AM";
+  }
+  alarmHours.toString();
+
+  var alarmMinutes = alarmTime.substring(3);
+
+  alarmTime = alarmHours + ":" + alarmMinutes + ":00 " + alarmM;
+  return alarmTime;
+}
+
 exports.alarmModule = Alarm;
